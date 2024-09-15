9e648a6bd988925a1127b1ffca4fd624c57d84e7
Faizan Ali <faizanali7654@gmail.com>
2024-09-15 19:19:02 +0530

    Add project files.

---
+++ PracticeQuestions/Program.cs
@@ -0,0 +1,235 @@
+﻿using System;
+
+namespace PracticeQuestions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Find the Number is Even Or Odd
+
+            //int num, reminder;
+            //Console.WriteLine("Please enter your number...");
+            //num = int.Parse(Console.ReadLine());
+
+            //reminder = num % 2;
+            //if(reminder == 0)
+            //{
+            //    Console.WriteLine($"The given number = {num} is even number...");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"The given number = {num} is odd nuber...");
+            //}
+            //Console.ReadLine();
+
+            #endregion Find the Number is Even Or Odd
+
+            #region Find the Number is Positive or Negative
+
+            //Console.Write("Please Enter Your Number = ");
+            //int num = int.Parse(Console.ReadLine());
+            //Console.WriteLine();
+
+            //if (num > 0)
+            //{
+            //    Console.WriteLine($"The given number {num} is Positive");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"The given number {num} is Negative");
+            //}
+            //Console.ReadLine();
+            #endregion Find the Number is Positive or Negative
+
+            #region Find the Number is Prime Or Not
+
+            //int num,i, ctr = 0;
+            //Console.Write("Please Enter your Number = ");
+            //Console.WriteLine();
+            //num = int.Parse(Console.ReadLine());
+
+            //for (i = 2; i <= num / 2 ;  i++)
+            //{
+            //    if(num % 2 == 0)
+            //        ctr++;
+            //}
+            //if(ctr == 0 && ctr != 1)
+            //{
+            //    Console.WriteLine($"The given number {num} is Prime Number.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"The given number {num} is not Prime Number.");
+            //}
+            //Console.ReadLine();
+            #endregion Find the Number is Prime Or Not
+
+            #region Check if the given Number is Palindrome or Not
+            //Console.WriteLine("Please Enter your number = ");
+            //int num = int.Parse(Console.ReadLine());
+
+            //int reminder, sum = 0;
+            //int temp = num;            
+
+            //while(num > 0)
+            //{
+            //    reminder = num % 10;
+            //    sum = (sum * 10) + reminder;
+            //    num = num / 10;
+            //}
+            //if(temp == sum)
+            //{
+            //    Console.WriteLine($"The given Number {sum} is Palindrome");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"The given Number {sum} is not Palindrome");
+            //}
+            //Console.ReadLine();
+            #endregion Check if the given Number is Palindrome or Not
+
+            #region Check if the given Number is Armstrong or Not
+            //Console.WriteLine("Please Enter your Name = ");
+            //int num = int.Parse(Console.ReadLine());
+
+            //int reminder, sum = 0;
+            //int temp = num;
+
+            //while (num > 0)
+            //{
+            //    reminder = num % 10;
+            //    sum = sum + (reminder * reminder * reminder);
+            //    num = num / 10;
+            //}
+            //if(temp == sum)
+            //{
+            //    Console.WriteLine($"The given number {sum} is Armstrong Number...");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"The given number {sum} is not Armstrong Number...");
+            //}
+            //Console.ReadLine();
+            #endregion Check if the given Number is Armstrong or Not
+
+            #region Find a Maximum of Two Numbers
+            //int num1, num2,reminder;
+
+            //Console.WriteLine("PLease Enter Your First Number = ");
+            //num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("PLease Enter Your Second Number = ");
+            //num2 = int.Parse(Console.ReadLine());
+
+            //if(num1 > num2)
+            //{
+            //    reminder = num1;
+            //}
+            //else
+            //{
+            //    reminder = num2;
+            //}
+            //Console.WriteLine($"The Maximum is {reminder}");
+            //Console.ReadLine();
+
+            #endregion Find a Maximum of Two Numbers
+
+            #region Find a Minimum of Two Numbers
+            //int num1, num2, num3;
+
+            //Console.WriteLine("Please Enter Your First Number");
+            //num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Please Enter Your Second Number");
+            //num2 = int.Parse(Console.ReadLine());
+
+            //if (num1 < num2)
+            //{
+            //    num3 = num1;
+            //}
+            //else
+            //{
+            //    num3 = num2;
+            //}
+            //Console.WriteLine($"The Minimum Number is {num3}");
+            //Console.ReadLine();
+            #endregion Find a Minimum of Two Numbers
+
+            #region Find a Maximum of Three  Numbers
+            //int num1,num2,num3,num4;
+
+            //Console.Write("Please Enter Your First Number = ");
+            //num1=int.Parse(Console.ReadLine());
+            //Console.Write("Please Enter Your Second Number = ");
+            //num2 = int.Parse(Console.ReadLine());
+            //Console.Write("Please Enter Your Third Number = ");
+            //num3 = int.Parse(Console.ReadLine());
+
+            //if(num1 > num2)
+            //{
+            //    num4 = num1;
+            //}
+            //else if(num2 > num3)
+            //{
+            //    num4 = num2;
+            //}
+            //else
+            //{
+            //    num4 = num3;
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine($"The Maximum of Three Number is {num4}");
+            //Console.ReadLine();
+            #endregion Find a Maximum of Three  Numbers
+
+            #region Find a Minimum of Three  Numbers
+            //int num1, num2, num3, num4;
+
+            //Console.Write("Please Enter Your First Number = ");
+            //num1=int.Parse(Console.ReadLine());
+            //Console.Write("Please Enter Your Second Number = ");
+            //num2 = int.Parse(Console.ReadLine());
+            //Console.Write("Please Enter Your Third Number = ");
+            //num3 = int.Parse(Console.ReadLine());
+
+            //if (num1 < num2)
+            //{
+            //    num4 = num1;
+            //}
+            //else if (num2 < num3)
+            //{
+            //    num4 = num2;
+            //}
+            //else
+            //{
+            //    num4 = num3;
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine($"The Minimum Number of Three is {num4}");
+            //Console.ReadLine();
+            #endregion Find a Minimum of Three  Numbers
+
+            #region Find a Factorial of a Number
+            //int num, i, fact = 1;
+
+            //Console.Write("Please Enter Your Number = ");
+            //num = int.Parse(Console.ReadLine());
+
+            //for (i = 1;  i <= num; i++)
+            //{
+            //    fact = fact * i;
+            //}
+
+            //Console.WriteLine($"Factorial of {num} is {fact}");
+            //Console.ReadLine();
+            #endregion Find a Factorial of a Number
+
+            #region Find a Fibonacci of a Number
+            
+            #endregion Find a Fibonacci of a Number
+
+            #region Find GCD of Two Numbers
+
+            #endregion Find GCD of Two Numbers
+        }
+    }
+}
